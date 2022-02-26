c036449ef63642ec42f12783d8eb7f1809796b9b
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-26 19:44:32 +0100

    add state for quote, add useEffect to handle api calls

--- front-end-development-libraries/random-quote-machine/src/App.js
+++ front-end-development-libraries/random-quote-machine/src/App.js
@@ -1,11 +1,42 @@
 import "./styles/App.css";
-
+import React from "react";
 import QuoteBox from "./components/QuoteBox";
 
 function App() {
+  const [quote, setQuote] = React.useState({
+    id: "",
+    text: "",
+    author: "",
+  });
+  const [quoteCount, setQuoteCount] = React.useState(0);
+
+  React.useEffect(
+    () =>
+      fetch("http://api.quotable.io/random")
+        .then((res) => res.json())
+        .then((x) =>
+          setQuote({
+            id: x._id,
+            text: x.content,
+            author: x.author,
+          })
+        ),
+    [quoteCount]
+  );
+
+  function handleRegenerate() {
+    setQuoteCount((prevCount) => prevCount + 1);
+  }
   return (
-    <main>
-      <QuoteBox />
+    <main className="main-container">
+      <div style={{ display: "grid", placeItems: "center" }}>
+        <h1 id="page-title">Random Quote Machine</h1>
+        <QuoteBox
+          author={quote.author}
+          quote={quote.text}
+          handleRegenerate={handleRegenerate}
+        />
+      </div>
     </main>
   );
 }
