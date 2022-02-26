6b64c130d6b537d63928327391930be65b9287d3
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-26 16:22:11 +0100

    style quote box, add fonts

--- front-end-development-libraries/random-quote-machine/src/components/QuoteBox.js
+++ front-end-development-libraries/random-quote-machine/src/components/QuoteBox.js
@@ -1,11 +1,17 @@
 import React from "react";
 
+import "../styles/QuoteBox.css";
+
 export default function QuoteBox() {
   return (
     <div id="quote-box">
-      <p id="text"></p>
-      <p id="author"></p>
-      <button id="new-quote">NEW</button>
+      <p id="text">
+        Remember no one can make you feel inferior without your consent.
+      </p>
+      <p id="author">Eleanor Roosevelt</p>
+      <button id="new-quote">
+        Remember no one can make you feel inferior without your consent.
+      </button>
       <a
         id="tweet-quote"
         href="https://twitter.com/intent/tweet"
