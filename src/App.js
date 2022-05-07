e36f5415db6857def67cf4fbb18da74847043133
Leda <ledathemis@protonmail.com>
2022-05-07 13:54:43 +0000

    use quotable API to fetch random quote

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import "./styles/App.css";
-import quotes from "./data/quotes.js";
-import React from "react";
-import QuoteBox from "./components/QuoteBox";
+import './styles/App.css';
+import React from 'react';
+import QuoteBox from './components/QuoteBox';
 
 function App() {
   const [quote, setQuote] = React.useState({
-    id: "",
-    text: "",
-    author: "",
+    id: '',
+    text: '',
+    author: '',
   });
   const [quoteCount, setQuoteCount] = React.useState(0);
 
+  const getRandomQuote = async () => {
+    const quoteData = await fetch('http://api.quotable.io/random');
+    const { _id, content, author } = await quoteData.json();
+    return { id: _id, text: content, author: author };
+  };
+
   React.useEffect(() => {
-    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    setQuote({
-      id: randomQuote._id,
-      text: randomQuote.content,
-      author: randomQuote.author,
-    });
+    (async () => {
+      const randomQuote = await getRandomQuote();
+      setQuote({
+        id: randomQuote.id,
+        text: randomQuote.text,
+        author: randomQuote.author,
+      });
+    })();
   }, [quoteCount]);
 
   function handleRegenerate() {
@@ -25,13 +32,9 @@ function App() {
   }
   return (
     <main className="main-container">
-      <div style={{ display: "grid", placeItems: "center" }}>
+      <div style={{ display: 'grid', placeItems: 'center' }}>
         <h1 id="page-title">Random Quote Machine</h1>
-        <QuoteBox
-          author={quote.author}
-          quote={quote.text}
-          handleRegenerate={handleRegenerate}
-        />
+        <QuoteBox author={quote.author} quote={quote.text} handleRegenerate={handleRegenerate} />
       </div>
     </main>
   );
